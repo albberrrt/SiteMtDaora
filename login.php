8cbb76d371466d04c18fcb8e889a8f7477250d45
Albert Smaczylo <albberrrt.yayay@gmail.com>
2022-06-11 00:47:56 -0300

    lalala

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="style.css" media="screen" type="text/css">
+    <link rel="stylesheet" href="style2.css" media="screen" type="text/css">
 
     <title>BigodeFlix - Entrar</title>
 
@@ -21,16 +21,17 @@
                 <h1>Entrar</h1>
                 <section id="secLogin">
                     <div class="input-div">
-                        <input type="text" id="inputUserName" class="inputLogin">
+                        <input type="text" id="inputUserName" class="inputLogin" placeholder=" " autocomplete="of">
                         <label for="inputUserName" class="placeholder-input">Email ou nome de usuário</label>
                     </div>
                     <div class="input-div">
-                        <input type="password" id="inputPasswords" class="inputLogin">
+                        <input type="password" id="inputPasswords" class="inputLogin" value="" placeholder=" ">
                         <label for="inputPassword" class="placeholder-input">Senha</label>
                     </div>
-                    <div>
+                    <div class="button-div">
                         <button type="submit">Entrar</button>
                     </div>
+                        <h5>Não possui uma conta? <a href="cadastro.php">Crie agora!!</a></h5>
                 </section>
             </form>
         </div>
