d95bb0fa52c27c41e4af07efb93581cbd032d23f
Albert Smaczylo <albberrrt.yayay@gmail.com>
2022-06-10 23:35:27 -0300

    botentar

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css" media="screen" type="text/css">
+
+    <title>BigodeFlix - Entrar</title>
+
+</head>
+<body>
+
+    <header>
+        <h1>BigodeFlix</h1>
+    </header>
+    <main>
+        <div id="content">
+            <form>
+                <h1>Entrar</h1>
+                <section id="secLogin">
+                    <div class="input-div">
+                        <input type="text" id="inputUserName" class="inputLogin">
+                        <label for="inputUserName" class="placeholder-input">Email ou nome de usuário</label>
+                    </div>
+                    <div class="input-div">
+                        <input type="password" id="inputPasswords" class="inputLogin">
+                        <label for="inputPassword" class="placeholder-input">Senha</label>
+                    </div>
+                    <div>
+                        <button type="submit">Entrar</button>
+                    </div>
+                </section>
+            </form>
+        </div>
+
+
+    </main>
+    <footer>
+
+    </footer>
+
+</body>
+</html>
